8997e4f130a6f655c11a986d753dc4e084ee76bd
Gachoki-kyu <gachoki.14044@students.kyu.ac.ke>
2024-12-22 14:33:09 +0300

    just a commit

--- apps/main.py
+++ apps/main.py
@@ -3,10 +3,19 @@ from .Routers import post,user, auth, votes
 from . import models
 from .database import engine
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
